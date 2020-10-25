7283ece7220f45433e01deeaca0e10bd522222d3
Alena Zaytseva <64847038+zaa19u450@users.noreply.github.com>
2020-10-25 18:26:54 +0300

    Add files via upload

---
+++ special/src/film.c
@@ -0,0 +1,150 @@
+#include "film.h"
+
+int film_create(struct film_struct *film, const char *title, const char *name, int year)
+{
+    int rc = OK;
+    char *title_temp = NULL;
+    char *name_temp = NULL;
+
+    title_temp = calloc(strlen(title) + 1, sizeof(char));
+    if (title_temp != NULL)
+    {
+        title_temp = strncpy(title_temp, title, strlen(title));
+        name_temp = calloc(strlen(name) + 1, sizeof(char));
+        if (name_temp != NULL)
+        {
+            name_temp = strcpy(name_temp, name);
+            free(film->title);
+            free(film->name);
+
+            film->title = title_temp;
+            film->name = name_temp;
+            film->year = year;
+        }
+        else
+        {
+            free(title_temp);
+            rc = ERRMEM;
+        }
+    }
+    else
+        rc = ERRMEM;
+
+    return rc;
+}
+
+void film_free_strings(struct film_struct *film)
+{
+    free(film->title);
+    film->title = NULL;
+    free(film->name);
+    film->name = NULL;
+}
+
+
+int film_read(FILE *f, struct film_struct *film)
+{
+    int rc = OK;
+
+    char *title = NULL;
+    char *name = NULL;
+    int year = 0;
+
+    size_t len = 0;
+
+    if ((rc = my_getdelim(&title, &len, '\n', f)) == OK)
+    {
+        if ((rc = my_getdelim(&name, &len, '\n', f)) == OK)
+        {
+            if ((fscanf(f, "%d", &year) == 1) && (fgetc(f) == '\n') && (year >= MINYEAR) && (year <= MAXYEAR))
+            {
+                rc = film_create(film, title, name, year);
+            }
+            else
+                rc = ERRVALUE;
+        }
+    }
+
+    free(title);
+    free(name);
+
+    return rc;
+}
+
+void film_print(FILE *f, struct film_struct *film)
+{
+    fprintf(f, "%s\n", film->title);
+    fprintf(f, "%s\n", film->name);
+    fprintf(f, "%d\n", film->year);
+}
+
+int film_cmp_title(const void *l, const void *r)
+{
+    struct film_struct *film_l = (struct film_struct *) l;
+    struct film_struct *film_r = (struct film_struct *) r;
+
+    size_t len_l = strlen(film_l->title);
+    size_t len_r = strlen(film_r->title);
+
+    if (len_l == len_r)
+        return strncmp(film_l->title, film_r->title, len_l);
+    else
+    {
+        if (len_l < len_r)
+        {
+            if (strncmp(film_l->title, film_r->title, len_l) <= 0)
+                return -1;
+            else
+                return 1;
+        }
+        else
+        {
+            if (strncmp(film_l->title, film_r->title, len_r) >= 0)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
+
+int film_cmp_name(const void *l, const void *r)
+{
+    struct film_struct *film_l = (struct film_struct *) l;
+    struct film_struct *film_r = (struct film_struct *) r;
+
+    size_t len_l = strlen(film_l->name);
+    size_t len_r = strlen(film_r->name);
+
+    if (len_l == len_r)
+        return strncmp(film_l->name, film_r->name, len_l);
+    else
+    {
+        if (len_l < len_r)
+        {
+            if (strncmp(film_l->name, film_r->name, len_l) <= 0)
+                return -1;
+            else
+                return 1;
+        }
+        else
+        {
+            if (strncmp(film_l->name, film_r->name, len_r) >= 0)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
+
+int film_cmp_year(const void *l, const void *r)
+{
+    struct film_struct *film_l = (struct film_struct *) l;
+    struct film_struct *film_r = (struct film_struct *) r;
+
+    return ((film_l->year) - (film_r->year));
+}
+
+int film_copy(struct film_struct *to, const struct film_struct *from)
+{
+    return film_create(to, from->title, from->name, from->year);
+}
---
+++ special/src/film_array.c
@@ -0,0 +1,87 @@
+#include "film_array.h"
+
+int films_count(FILE *f, int *n)
+{
+    int rc = OK;
+    struct film_struct film;
+    memset(&film, 0, sizeof(struct film_struct));
+    *n = 0;
+
+    while ((!feof(f)) && (rc == OK))
+    {
+        rc = film_read(f, &film);
+        if (rc == OK)
+        {
+            (*n)++;
+            film_free_strings(&film);
+        }
+    }
+    if ((*n) == 0)
+        rc = ERREMPTY;
+    else if (rc == END)
+    {
+        rc = OK;
+    }
+    return rc;
+}
+
+int films_change(struct film_struct *film1, struct film_struct *film2)
+{
+    struct film_struct film_temp;
+    memset(&film_temp, 0, sizeof(struct film_struct));
+    if (film_copy(&film_temp, film1) || film_copy(film1, film2) || film_copy(film2, &film_temp))
+    {
+        return ERRMEM;
+    }
+
+    film_free_strings(&film_temp);
+    return OK;
+}
+
+void films_print(FILE *f, struct film_struct *films, int n)
+{
+    for (int i = 0; i < n; i++)
+        film_print(f, (films + i));
+}
+
+int films_read_sort(FILE *f, struct film_struct *films, int n,
+                     int (*film_cmp)(const void *, const void *))
+{
+    int rc = OK;
+    for (int i = 0; (i < n) && (rc == OK); i++)
+    {
+        film_read(f, films + i);
+        for (int j = i; (j > 0) && (film_cmp((films + j), (films + (j - 1))) < 0); j--)
+            rc = films_change((films + j), (films + (j - 1)));
+    }
+    return rc;
+}
+
+int films_search(struct film_struct *films, int n, struct film_struct *goal,
+                 int (*film_cmp)(const void *, const void *))
+{
+    int low, high, middle;
+    low = 0;
+    high = n - 1;
+    while (low <= high)
+    {
+        middle = (low + high) / 2;
+        if (film_cmp(goal, films+middle) < 0)
+            high = middle - 1;
+        else if (film_cmp(goal, films+middle) > 0)
+            low = middle + 1;
+        else
+            return middle;
+    }
+    return ERRNOTFOUND;
+}
+
+void films_free(struct film_struct *films, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if ((films + i))
+            film_free_strings((films + i));
+    }
+    free(films);
+}
