2368dcace5ca58725a9e98f51f4989998464149c
Alena Zaytseva <64847038+zaa19u450@users.noreply.github.com>
2020-10-25 18:28:15 +0300

    Add files via upload

---
+++ special/unit_tests/check_main.c
@@ -0,0 +1,29 @@
+#include "check_main.h"
+
+int main(void)
+{
+    int no_failed = 0;
+    SRunner *sr;
+    sr = srunner_create(film_create_suite());
+    srunner_add_suite(sr, film_free_strings_suite());
+    srunner_add_suite(sr, film_read_suite());
+    srunner_add_suite(sr, film_cmp_title_suite());
+    srunner_add_suite(sr, film_cmp_name_suite());
+    srunner_add_suite(sr, film_cmp_year_suite());
+    srunner_add_suite(sr, film_copy_suite());
+
+    srunner_add_suite(sr, my_getdelim_suite());
+
+    srunner_add_suite(sr, films_count_suite());
+    srunner_add_suite(sr, films_change_suite());
+    srunner_add_suite(sr, films_read_sort_suite());
+    srunner_add_suite(sr, films_search_suite());
+
+
+    srunner_run_all(sr, CK_VERBOSE);
+    no_failed = srunner_ntests_failed(sr);
+
+    srunner_free(sr);
+
+    return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
